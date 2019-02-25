916b681489738cef4e2c3e7b73cc55ef60b01538
matech96 <gangoly96@gmail.com>
2019-02-25 12:11:23 +0100

    init

---
+++ 000_initial/js/Scene.js
@@ -0,0 +1,37 @@
+"use strict";
+const Scene = function(gl) {
+  this.vsQuad = new Shader(gl, gl.VERTEX_SHADER, "quad_vs.essl");
+  this.fsTrace = new Shader(gl, gl.FRAGMENT_SHADER, "trace_fs.essl");
+  this.traceProgram = new TexturedProgram(gl, this.vsQuad, this.fsTrace);
+  this.quadGeometry = new TexturedQuadGeometry(gl);  
+
+  this.timeAtFirstFrame = new Date().getTime();
+  this.timeAtLastFrame = this.timeAtFirstFrame;
+
+  this.camera = new PerspectiveCamera();
+};
+
+Scene.prototype.update = function(gl, keysPressed) {
+  //jshint bitwise:false
+  //jshint unused:false
+  const timeAtThisFrame = new Date().getTime();
+  const dt = (timeAtThisFrame - this.timeAtLastFrame) / 1000.0;
+  const t = (timeAtThisFrame - this.timeAtFirstFrame) / 1000.0; 
+  this.timeAtLastFrame = timeAtThisFrame;
+
+  // clear the screen
+  gl.clearColor(0.3, 0.0, 0.3, 1.0);
+  gl.clearDepth(1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  gl.clearColor(0.6, 0.0, 0.3, 1.0);
+  gl.clearDepth(1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  this.camera.move(dt, keysPressed);
+
+  this.traceProgram.commit();
+  this.quadGeometry.draw();
+};
+
+
